f745a60d5409a3db91d85bb81c25ed39ed88acd7
janerist <jan.erik.strom@gmail.com>
2018-10-30 20:35:49 +0100

    Add --directory option

--- git_kahoot.py
+++ git_kahoot.py
@@ -1,5 +1,26 @@
 import click
+import subprocess
+from subprocess import CalledProcessError
+import os
+
+
+def validate_git_repo(ctx, param, value):
+    try:
+        git_dir = subprocess.check_output(
+            ['git', 'rev-parse', '--git-dir'], universal_newlines=True, cwd=value)
+        return os.path.abspath(git_dir)
+    except CalledProcessError:
+        raise click.BadParameter(
+            f'Directory "{value}" not a git repository.')
+
 
 @click.command()
-def cli():
-    click.echo('Hello World!')
+@click.option('-d',
+              '--directory',
+              default='.',
+              required=True,
+              type=click.Path(exists=True, file_okay=False),
+              callback=validate_git_repo
+              )
+def cli(directory):
+    click.echo(f'Using git repository {directory}')
