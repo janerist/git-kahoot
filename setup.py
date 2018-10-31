80242098484e1decc8549b3da57307b30b1fc4ee
janerist <jan.erik.strom@gmail.com>
2018-10-31 19:47:17 +0100

    Ready for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='git-kahoot',
-    version='0.1',
-    py_modules=['cli'],
+    version='0.1.2',
+    author='Jan-Erik Strøm',
+    author_email='jan.erik.strom@gmail.com',
+    description='Generates Kahoot quiz from commits in a git repository',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/janerist/git-kahoot',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    py_modules=['cli', 'git', 'kahoot'],
     install_requires=[
         'Click',
         'requests'
