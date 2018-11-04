295730bcb404b55ecd92a83ccea6e443627255bb
janerist <jan.erik.strom@gmail.com>
2018-11-04 17:29:41 +0100

    Allow multiple git repositories

--- cli.py
+++ cli.py
@@ -3,27 +3,66 @@ import kahoot
 import git
 import random
 from itertools import islice, cycle
+import os
 
 
-def validate_git_repo(ctx, param, value):
+def validate_git_repo(ctx, param, repos):
     try:
-        return git.get_git_dir(value)
+        return [git.get_git_dir(repo) for repo in repos]
     except git.GitError as err:
         raise click.BadParameter(err)
 
 
 @click.command()
-@click.option('-d', '--directory', default='.', help='path to git repository (default: current directory)',
-              type=click.Path(exists=True, file_okay=False), callback=validate_git_repo)
-@click.option('--since', help='only include commits since this date', type=click.DateTime())
-@click.option('--until', help='only include commits up until this date', type=click.DateTime())
-@click.option('-n', '--count', default=20, help='number of questions (default: 20, max: 100)',
-              type=click.IntRange(min=1, max=100))
-@click.option('-u', '--username', required=True, prompt='Kahoot user name', help='Kahoot user name')
-@click.option('-p', '--password', required=True, prompt='Kahoot password', hide_input=True, help='Kahoot password')
-@click.option('-t', '--title', default='Git Commiter Quiz',
-              help='title of the generated quiz (default: "Git Commiter Quiz"')
-def cli(directory, since, until, count, username, password, title):
+@click.option(
+    '-d',
+    '--directory',
+    'repos',
+    default='.',
+    multiple=True,
+    help='path to git repository (default: current directory)',
+    type=click.Path(exists=True, file_okay=False),
+    callback=validate_git_repo
+)
+@click.option(
+    '--since',
+    help='only include commits since this date',
+    type=click.DateTime()
+)
+@click.option(
+    '--until',
+    help='only include commits up until this date',
+    type=click.DateTime()
+)
+@click.option(
+    '-n',
+    '--count',
+    default=20,
+    help='number of questions (default: 20, max: 100)',
+    type=click.IntRange(min=1, max=100)
+)
+@click.option(
+    '-u',
+    '--username',
+    required=True,
+    prompt='Kahoot user name',
+    help='Kahoot user name'
+)
+@click.option(
+    '-p',
+    '--password',
+    required=True,
+    prompt='Kahoot password',
+    hide_input=True,
+    help='Kahoot password'
+)
+@click.option(
+    '-t',
+    '--title',
+    default='Git Commiter Quiz',
+    help='title of the generated quiz (default: "Git Commiter Quiz"'
+)
+def cli(repos, since, until, count, username, password, title):
     """Generates Kahoot quiz from commits in a git repository."""
 
     if since is not None and until is not None and since >= until:
@@ -34,7 +73,8 @@ def cli(directory, since, until, count, username, password, title):
     except kahoot.KahootError as err:
         raise click.UsageError(err)
 
-    click.echo(f'Using git repository {directory}')
+    for repo in repos:
+        click.echo(f'Using git repository {repo}')
     if since is not None:
         click.echo(f'Only including commits since {since}')
     if until is not None:
@@ -43,8 +83,14 @@ def cli(directory, since, until, count, username, password, title):
 
     click.echo('Getting commits...')
 
-    authors = git.get_authors(directory)
-    commits = git.get_random_commits(directory, since, until, count)
+    repodata = [{'name': os.path.basename(os.path.abspath(os.path.join(repo, '..'))),
+                 'authors': git.get_authors(repo),
+                 'commits': git.get_random_commits(repo, since, until, count)} for repo in repos]
+
+    questions = [create_question(repo['name'] if len(repos) > 1 else None, repo['authors'], repo['commits'].pop())
+                 for repo in islice(cycle(repodata), None, count)]
+
+    random.shuffle(questions)
 
     click.echo('Creating quiz...')
 
@@ -56,7 +102,7 @@ def cli(directory, since, until, count, username, password, title):
         'language': 'English',
         'audience': 'Social',
         'coverMetadata': {},
-        'questions': [create_question(commit, authors) for commit in commits]
+        'questions': questions
     }
 
     quiz_id = kahoot.create_quiz(quiz, access_token)
@@ -64,7 +110,7 @@ def cli(directory, since, until, count, username, password, title):
     click.echo(f'Success! Your quiz is ready at: https://play.kahoot.it/#/?quizId={quiz_id}')
 
 
-def create_question(commit, authors):
+def create_question(repo_name, authors, commit):
     author, date, message = commit
     choices = []
     if len(authors) < 4:
@@ -78,8 +124,11 @@ def create_question(commit, authors):
 
     random.shuffle(choices)
 
+    question = f'[{repo_name}] ' if repo_name else ''
+    question += f'({date}):\n{message}'
+
     return {
-        'question': message,
+        'question': question,
         'questionFormat': 0,
         'time': 20000,
         'points': True,
